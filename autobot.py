92dab52835d3c0f58ddd81181f52855a199d941c
adsikramlink <adsikramlink@gmail.com>
2025-12-28 12:01:08 +0800

    Update autobot.py

--- autobot.py
+++ autobot.py
@@ -15,7 +15,7 @@ time.sleep(delay_detik)
 
 # 2. Setup AI
 genai.configure(api_key=os.environ['GEMINI_API_KEY'])
-model = genai.GenerativeModel('gemini-2.5-flash')
+model = genai.GenerativeModel('gemini-3-flash-preview')
 
 def get_random_topic():
     topik_list = [
